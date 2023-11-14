e76cbedb5ddf36bbc6dbf4e83b58b41e40a4909b
andbardii <andbardii@icloud.com>
2023-11-14 19:09:08 +0100

    add modal and filters

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// BASICS
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -11,3 +12,67 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// MODALE
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+var openBtns = document.getElementsByClassName("openBtn");
+for (var i = 0; i < openBtns.length; i++) {
+  openBtns[i].addEventListener("click", function() {
+    var iframeSrc = this.getAttribute("data-src");
+    loadModalWithIframe(iframeSrc);
+  });
+}
+
+function loadModalWithIframe(iframeSrc) {
+  var iframe = document.querySelector("#myModal iframe");
+  iframe.src = iframeSrc;
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// FILTRI
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('.card');
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+        filterButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                const filterValue = this.textContent.trim().toLowerCase();
+                
+                // Nascondi tutti gli elementi
+                portfolioItems.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                // Mostra solo gli elementi che corrispondono alla categoria selezionata
+                if (filterValue === 'all') {
+                    portfolioItems.forEach(item => {
+                        item.style.display = 'block';
+                    });
+                } else {
+                    const filteredItems = document.querySelectorAll(`.portfolio-item.${filterValue}`);
+                    filteredItems.forEach(item => {
+                        item.style.display = 'block';
+                    });
+                }
+            });
+        });
+});
+
+
+
+
+
+
+
